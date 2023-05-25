7c132a5cd7750cc2efc3a61c3d8983ff28de4589
Sven Aelterman <17446043+SvenAelterman@users.noreply.github.com>
2023-05-25 08:14:23 -0500

    Revert demo change

--- simple.ps1
+++ simple.ps1
@@ -1,2 +1,2 @@
-New-AzResourceGroupDeployment -ResourceGroupName 'rg-bicep-demo-eastus2-01' -TemplateFile .\03-param.bicep `
+New-AzResourceGroupDeployment -ResourceGroupName 'rg-bicep-demo-eastus2-01' -TemplateFile .\01-simple.bicep `
 	-DeploymentName "simple-$((Get-Date).ToString("yyyyMMdd-hhmmss"))"
